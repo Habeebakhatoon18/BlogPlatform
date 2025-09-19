1b9fb8fdcd3cccce320dc02684df477ee53c2e16
Habeeba Khatoon <Khabeeba18@gmail.com>
2025-09-19 23:01:40 +0530

    searching & blog functions

--- src/components/BlogPostSection.jsx
+++ src/components/BlogPostSection.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button } from '@material-tailwind/react';
+import myContext from '../../context/data/myContext';
 
-const BlogPostSection = () => {
+import { useNavigate, Link } from 'react-router-dom';
+
+const BlogPostsection = () => {
+  const context = useContext(myContext);
+  const { mode, getAllBlog } = context;
+  const navigate = useNavigate();
+  const stripHtml = (html) => html.replace(/<\/?[^>]+(>|$)/g, "");
   return (
-    <div>BlogPostSection</div>
+    <section className=' container p-4 mx-auto '>
+      <h1 className='text-center' style={{
+        color: mode === 'dark'
+          ? 'rgb(226, 232, 240)'
+          : ' rgb(30, 41, 59)'
+      }}>Latest Blogs </h1>
+      <div className=' flex m-4 p-3 justify-center items-center w-full flex-wrap'>
+
+        {getAllBlog.length > 0
+          ?
+          <>
+            {getAllBlog.slice(-6).map((item, index) => {
+              const { thumbnail, id, date } = item
+              return (
+                <div className="p-4 md:w-1/3 md:h-[550px]" key={index}>
+                  <div onClick={() => navigate(`/bloginfo/${id}`)}
+                    style={{
+                      background: mode === 'dark'
+                        ? 'rgb(30, 41, 59)'
+                        : 'white',
+                      borderBottom: mode === 'dark'
+                        ?
+                        ' 4px solid rgb(226, 232, 240)'
+                        : ' 4px solid rgb(30, 41, 59)'
+                    }}
+                    className={`h-full shadow-lg  hover:-translate-y-1 cursor-pointer hover:shadow-gray-400
+                        ${mode === 'dark'
+                        ? 'shadow-gray-700'
+                        : 'shadow-xl'
+                      } 
+                        rounded-xl overflow-hidden`}
+                  >
+                    {/* Blog Thumbnail  */}
+                    <img className=" w-full md:h-[200px]" src={thumbnail} alt="blog" />
+                    {/* Top Items  */}
+                    <div className="p-6">
+                      {/* Blog Date  */}
+                      <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{
+                        color: mode === 'dark'
+                          ? 'rgb(226, 232, 240)'
+                          : ' rgb(30, 41, 59)'
+                      }}>
+                        {date}
+                      </h2>
+                      {/* Blog Title  */}
+                      <h1 className="title-font text-lg font-bold text-gray-900 mb-3" style={{
+                        color: mode === 'dark'
+                          ? 'rgb(226, 232, 240)'
+                          : ' rgb(30, 41, 59)'
+                      }}>
+                        {item.title}
+                      </h1>
+                      {/* Blog Description  */}
+
+
+                      <p
+                        className="leading-relaxed mb-3"
+                        style={{
+                          color: mode === 'dark' ? 'rgb(226, 232, 240)' : 'rgb(30, 41, 59)'
+                        }}
+                        dangerouslySetInnerHTML={{
+                          __html:
+                            stripHtml(item.content).length > 150
+                              ? stripHtml(item.content).slice(0, 150) + "..."
+                              : stripHtml(item.content)
+                        }}
+                      ></p>
+
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </>
+          :
+          <>
+            <h1 className='text-xl font-bold'>No Blogs Found</h1>
+          </>
+        }
+
+
+      </div>
+      <div className='flex justify-center my-5'>
+        {/* See More Button  */}
+        <Link to={'/allblogs'}>
+          <Button
+            style={{
+              background: mode === 'dark'
+                ? 'rgb(226, 232, 240)'
+                : 'rgb(30, 41, 59)',
+              color: mode === 'dark'
+                ?
+                'rgb(30, 41, 59)'
+                : 'rgb(226, 232, 240)'
+            }}>
+            See More
+          </Button>
+        </Link>
+      </div>
+
+    </section>
   )
 }
 
-export default BlogPostSection
+export default BlogPostsection
--- src/components/SearchDialog.jsx
+++ src/components/SearchDialog.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext, useState } from "react";
 import { Dialog, DialogBody, Input,} from "@material-tailwind/react";
-import myContext from "../context/data/myContext";
+import myContext from "../../context/data/myContext";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useNavigate } from "react-router";
 
@@ -21,9 +21,54 @@ export default function SearchDialog() {
                 <AiOutlineSearch size={20} color="white" />
             </div>
             {/* Dialog  */}
+            <Dialog className=" flex justify-center  items-center fixed inset-0 w-[25em] h-[18em] md:h-[25em] m-auto mt-0" open={open} handler={handleOpen} style={{ background: mode === 'light' ? '#2f3542' : '#2f3542', color: mode === 'dark' ? 'white' : 'black' }}>
+                {/* Dialog Body  */}
+                <DialogBody  >
+                    <div className="flex w-full   justify-center">
+                        {/* Input  */}
+                        <Input
+                            color="white"
+                            type="search"
+                            label="Type here..."
+                            value={searchkey}
+                            onChange={(e) => setSearchkey(e.target.value)}
+                            className=" bg-[#2C3A47]"
+                            name="searchkey"
+                            containerProps={{
+                                className: "md:min-w-[288px]",
+                            }}
+                        />
+                    </div>
 
-           <Dialog></Dialog>
-            
+                    {/* Blog Card  */}
+                    <div className="flex justify-center flex-wrap sm:mx-auto sm:mb-2 -mx-2 mt-4 mb-2">
+                        {
+                            getAllBlog.filter((obj) => obj.title.toLowerCase().includes(searchkey)).map((item, index) => {
+
+                                return (
+                                    <div key={index} className=" sm:w-1/4 w-full " >
+                                        <div onClick={() => naviagte(`/bloginfo/${item.id}`)} className=" container cursor-pointer mx-auto px-4 bg-gray-200 p-2 rounded-lg ">
+                                            {/* Blog Thumbnail  */}
+                                            <img className="w-20 mb-2 rounded-lg"
+                                                src={item.thumbnail} alt="" />
+
+                                            {/* Blog Date  */}
+                                            <p className="w-40 text-sm">{item.date}</p>
+
+                                            {/* Blog Title  */}
+                                            <h3>{item.title}</h3>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                    </div>
+
+                    {/* Heading  */}
+                    <div className=" text-center">
+                        <h1 className=" text-gray-400">Powered By Tech Diva</h1>
+                    </div>
+                </DialogBody>
+            </Dialog>
         </Fragment>
     );
 }
